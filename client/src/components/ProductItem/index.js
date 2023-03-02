f04e67e3955b6e0852dbd1c2b7e292053efc248d
hannybear88 <hannahkchung88@gmail.com>
2023-03-02 10:31:28 -0800

    modified code and added comments in client/src/components/ProductItem/index.js file

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
-import { useStoreContext } from "../../utils/GlobalState";
+// commented out in favor of redux logic
+// import { useStoreContext } from "../../utils/GlobalState";
+import { useDispatch, useSelector } from 'react-redux';
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 
 function ProductItem(item) {
-  const [state, dispatch] = useStoreContext();
+  // commented out in favor of redux logic
+  // const [state, dispatch] = useStoreContext();
+  const state = useSelector((state) => {
+    return state
+  });
+  const dispatch = useDispatch();
 
+  const { cart } = state;
+    
   const {
     image,
     name,
@@ -16,7 +25,7 @@ function ProductItem(item) {
     quantity
   } = item;
 
-  const { cart } = state
+  
 
   const addToCart = () => {
     const itemInCart = cart.find((cartItem) => cartItem._id === _id)
