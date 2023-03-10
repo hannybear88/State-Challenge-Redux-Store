cad59662ba0b280520ee26287a6d92154956d7f5
hannybear88 <hannahkchung88@gmail.com>
2023-03-09 18:27:37 -0800

    modified client/src/pages/Home.js and Login.js files

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState }from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
 
+ // after being managed globally
 const Home = () => {
   return (
     <div className="container">
