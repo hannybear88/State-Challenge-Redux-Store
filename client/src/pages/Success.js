87f0c96f1296afb47225e9fd849287514f9575b7
hannybear88 <hannahkchung88@gmail.com>
2023-03-09 22:08:47 -0800

    modified code in client/src/pages/Success.js file

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useMutation } from '@apollo/client';
+import { useMutation } from '@apollo/react-hooks';
 import Jumbotron from '../components/Jumbotron';
 import { ADD_ORDER } from '../utils/mutations';
 import { idbPromise } from '../utils/helpers';
@@ -33,8 +33,12 @@ function Success() {
     <div>
       <Jumbotron>
         <h1>Success!</h1>
-        <h2>Thank you for your purchase!</h2>
-        <h2>You will now be redirected to the home page</h2>
+        <h2>
+          Thank you for your purchase!
+        </h2>
+        <h2>
+          You will now be redirected to the home page
+        </h2>
       </Jumbotron>
     </div>
   );
