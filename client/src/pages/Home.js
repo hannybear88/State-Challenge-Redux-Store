b64e2a3116366d4e676d04d22fca37fd23a15efb
hannybear88 <hannahkchung88@gmail.com>
2023-03-13 23:19:17 -0700

    added more code, but commented out in client/src/pages/Home.js file

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,22 @@ import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
 
+// Before being managed globally
+// const Home = () => {
+//   const [currentCategory, setCategory] = useState("");
+
+//   before being managed globally
+//   return (
+//     <div className="container">
+//       <CategoryMenu setCategory={setCategory} />
+//       <ProductList currentCategory={currentCategory} />
+//     </div>
+//   );
+
+ 
+// };
+
+
 // after being managed globally
 const Home = () => {
   return (
