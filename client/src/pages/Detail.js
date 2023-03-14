fd7b4e3332a41da588d96896f12096710934eb4b
hannybear88 <hannahkchung88@gmail.com>
2023-03-13 23:16:29 -0700

    modified code in client/src/pages/Detail.js

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/react-hooks';
+// import { useQuery } from '@apollo/client';
 
-import Cart from '../components/Cart';
+import { QUERY_PRODUCTS } from '../utils/queries';
+import spinner from '../assets/spinner.gif';
+
 import { useStoreContext } from '../utils/GlobalState';
 import {
   REMOVE_FROM_CART,
@@ -10,9 +13,11 @@ import {
   ADD_TO_CART,
   UPDATE_PRODUCTS,
 } from '../utils/actions';
-import { QUERY_PRODUCTS } from '../utils/queries';
+
+import Cart from '../components/Cart';
 import { idbPromise } from '../utils/helpers';
-import spinner from '../assets/spinner.gif';
+
+import { useDispatch, useSelector } from 'react-redux';
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
