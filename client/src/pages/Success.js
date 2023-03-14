1082a136682dc0d1b20fcceb87884875cf233304
hannybear88 <hannahkchung88@gmail.com>
2023-03-13 23:28:18 -0700

    modified code in client/src/pages/Success.js

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
-import { useMutation } from '@apollo/client';
+import { useMutation } from '@apollo/react-hooks';
+// import { useMutation } from '@apollo/client';
+
 import Jumbotron from '../components/Jumbotron';
 import { ADD_ORDER } from '../utils/mutations';
 import { idbPromise } from '../utils/helpers';
