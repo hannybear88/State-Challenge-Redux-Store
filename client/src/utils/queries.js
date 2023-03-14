19c15b2ede7bdc0dc5e3574895d4199cb03ae79d
hannybear88 <hannahkchung88@gmail.com>
2023-03-13 23:42:14 -0700

    modified code in client/src/utils/queries.js file

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -16,14 +16,6 @@ export const QUERY_PRODUCTS = gql`
   }
 `;
 
-export const QUERY_CHECKOUT = gql`
-  query getCheckout($products: [ID]!) {
-    checkout(products: $products) {
-      session
-    }
-  }
-`;
-
 export const QUERY_ALL_PRODUCTS = gql`
   {
     products {
@@ -68,3 +60,17 @@ export const QUERY_USER = gql`
     }
   }
 `;
+
+export const QUERY_CHECKOUT = gql`
+  query getCheckout($products: [ID]!) {
+    checkout(products: $products) {
+      session
+    }
+  }
+`;
+
+
+
+
+
+
