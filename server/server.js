91375705e1c9c266bfd519058202287b991ab302
hannybear88 <hannahkchung88@gmail.com>
2023-03-14 00:33:27 -0700

    modified code in server/server.js file

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -13,6 +13,8 @@ const server = new ApolloServer({
   resolvers,
   context: authMiddleware,
 });
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
